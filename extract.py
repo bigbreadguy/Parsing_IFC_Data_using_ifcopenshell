59bdca8dca427ae89855a6733010a514dd667300
bigbreadguy <schadenlim@gmail.com>
2020-11-05 14:00:27 +0900

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,4 +1,4 @@
-def extract_dimension(ifc_file = ifc_file, ifc_type : str):
+def extract_dimension(ifc_type : str, ifc_file = ifc_file):
 
     if ifc_type=="IfcCovering":
         df = pd.DataFrame(columns = ["Name","IFC_Type", "Perimeter", "Area", "Volume"])
